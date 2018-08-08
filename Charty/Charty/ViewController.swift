66140d4eb4d49fcf3d15474400083d4f35617f2d
Nina Pratiwi <nina.pratiwi.02@gmail.com>
2018-08-09 09:34:38 +1000

    initial project

---
+++ Charty/Charty/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Charty
+//
+//  Created by Nina Pratiwi on 9/8/18.
+//  Copyright © 2018 Nina Pratiwi. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
